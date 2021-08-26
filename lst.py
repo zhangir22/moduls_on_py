8d581fd953225c07881b432c504196fee8466193
zhangir22 <62081080+zhangir22@users.noreply.github.com>
2021-08-26 21:04:46 +0600

    Add files via upload

--- lst.py
+++ lst.py
@@ -1,8 +1,75 @@
 import random as rnd
 def show_list(lst):
     for i in lst:
-        print(i,end=" ,")
+        print(i,end=" | ")
     print("\n")
+#
+def show_dictionary(dictionary):
+    for i in dictionary:
+        print(f"{i} = {dictionary[i]}")
+#
+def __find_key(dictionary, value):
+    for i in dictionary:
+        if dictionary[i] == value:
+            return i
+#
+def __check_key(dictionary,value):
+    return value in dictionary
+#
+def find_element_dict(dictionary,value):
+    for i in dictionary:
+        if dictionary[i]==value:
+            return True
+    return False
+#
+def add_more_elements(lst,values):
+    for i in values:
+        lst.append(i)
+#
+def add_element(lst,value):
+    lst.append(value)
+#
+def update_dictionary(dictionary,key,value):
+    if key:
+        if value:
+            dictionary.update(key,value)
+    else:
+        return "Параметры были проигнорированы"
+#
+def add_inside(lst,index,value):
+    if index>0 and index < len(lst):
+        lst.insert(index,value)
+#
+def remove_element(lst,index):
+    if index!=None or index>=0 and index<len(lst):
+        lst.pop(index)
+#
+def get_key_dictionary(dictionary,key):
+    for i in dictionary:
+        if i == key:
+            return i
+    return "Такого ключа нет"
+#
+def get_keys_dictionary(dictionary):
+    keys = list(dictionary.keys())
+    keys.sort()
+    return keys
+#
+def find_duplicate(lst,value):
+    indexes = []
+    for i in range(len(lst)):
+        if value == lst[i]:
+            indexes.append(i)
+    return indexes
+#
+def join_list(lst,value):
+    lst.extend(value)
+#
+def find_element(lst,value):
+    for i in range(len(lst)):
+        if lst[i] == value:
+            return i
+#
 def sorted_buble(values):
     for i in range(len(values)):
         for y in range(len(values) - 1, i ,-1):
@@ -10,12 +77,25 @@ def sorted_buble(values):
             if values[index] > values[y]:
                 values[index],values[y] = values[y],values[index]
     return values
-def create_list(count_elements):
+#
+def sorted_chose(values):
+    for i in range(len(values)):
+        index = len(values)
+        j = index - 1
+        while index > 0 and j > 0:
+            index-=1
+            j -=1
+            if values[index] < values[j] or values[index] == values[j]:
+                values[j],values[index] = values[index],values[j]
+        index-=i
+    return values
+#
+def create_list(count_elements,min_number,max_number):
     lst = []
-    max_number = 100
     for i in range(count_elements):
-        lst.append(rnd.randint(1,max_number))
+        lst.append(rnd.randint(min_number,max_number))
     return lst
+#
 def search_binary(lst,user_number):
     def cut_number(value_a, value_b):
         cut = 2
